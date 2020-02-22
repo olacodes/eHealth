ad5cbdadefdfd698ee90e19301caa6b352ec30e9
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-02-22 11:01:44 +0100

    initial setup

---
+++ eHealth/eHealth/db.py
@@ -0,0 +1,43 @@
+import dj_database_url
+from decouple import config
+
+
+class DB:
+    @classmethod
+    def config(cls, debug):
+        return cls.development() if debug else cls.production()
+
+    @classmethod
+    def production(cls):
+        return {
+            'default': {
+                **dj_database_url.parse(config('DATABASE_URL')),
+                'ENGINE': 'django.db.backends.postgresql'
+            }
+        }
+
+    # LOCAL ENV
+    # @classmethod
+    # def development(cls):
+    #     return {
+    #         'default': {
+    #             'ENGINE': 'django.db.backends.postgresql',
+    #             'NAME': 'postgres',
+    #             'HOST': '127.0.0.1',
+    #             'USER': 'postgres',
+    #             'PASSWORD': 'olatunde123',
+    #         }
+    #     }
+        
+    # DOCKER
+    @classmethod
+    def development(cls):
+        return {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'eHealthdb',
+                'HOST': 'db',
+                'USER': config('DBUSER'),
+                'PASSWORD': config('DBPASSWD'),
+            }
+        }
