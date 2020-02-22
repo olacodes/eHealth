a7c71754527f7f63f5f28bca00067c6080731e6e
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-02-22 16:38:12 +0100

    create docker convenient script

--- eHealth/eHealth/settings.py
+++ eHealth/eHealth/settings.py
@@ -14,6 +14,7 @@ import os
 from .db import DB
 from datetime import timedelta
 from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
