e799f858f7c3e83baf1834c4b3e1d555fabcf448
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-02-24 15:22:00 +0100

    create register page

--- eHealth/wecare/urls.py
+++ eHealth/wecare/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import (
     user_register, 
-    practitioner_register,
     index,
-    med_info_view,
     dashboard,    
 )
 
@@ -11,6 +9,5 @@ urlpatterns = [
     path('', index.Index.as_view(), name='index'),
     path('dashboard/', dashboard.Dashboard.as_view(), name='dashboard'),
     path('register/', user_register.UserFormView.as_view(), name='register'),
-    path('medical-information', med_info_view.med_info_view, name='medical-information'),
-    path('practitioner/register', practitioner_register.practitioner_register, name='practitioner-register'),
+    
 ]
