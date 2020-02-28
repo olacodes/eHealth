e1f1da6eb395bb6bfa737cf09a8942c57e485cea
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-02-28 23:50:41 +0100

    create homepage

--- eHealth/wecare/urls.py
+++ eHealth/wecare/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import (
     dashboard,
+    home,
+    user_information 
 )
 
-
 urlpatterns = [
+    path('', home.Home.as_view(), name='home'),
     path('dashboard/', dashboard.Dashboard.as_view(), name='dashboard'),
+    path('users/', user_information.UsersInformation.as_view(), name='users'),
 ]
